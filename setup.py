df02d0740990a73795308756af59812c71cec413
kanywst <ta1234kuma@gmail.com>
2020-07-20 03:14:24 +0900

    fix

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     entry_points = {
-        'console_scripts': ['nkap = nkap.nkap:main']
+        'console_scripts': ['src.nkap = nkap.nkap:main']
     },
     python_requires='>=3.6',
 )
